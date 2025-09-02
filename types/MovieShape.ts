4c728db32d179c24648f72104f1f02d4b951b000
Josue Marchena <josuemarchena0730@gmail.com>
2025-09-02 10:15:16 -0500

    Seccion footer terminada

---
+++ types/MovieShape.ts
@@ -0,0 +1,9 @@
+export type MovieShape = {
+    id: number;
+    title: string;
+    imgBackground: string;
+    imgTitle: string;
+    tags: string[];
+    description: string;
+    imgModal: string;
+}
