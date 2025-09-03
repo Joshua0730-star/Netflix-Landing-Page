d43f14f3f0394857ad4ac25c15d1864942d1c549
Josue Marchena <josuemarchena0730@gmail.com>
2025-09-03 08:38:23 -0500

    fix de issues con rutas estaticas y dinamicas

---
+++ src/lib/images.ts
@@ -0,0 +1,54 @@
+// IMAGES DE BACKGROUND DE LA MODA
+import img01 from "../assets/img-modals/img-01-modal.webp"
+import img02 from "../assets/img-modals/img-02-modal.webp"
+import img03 from "../assets/img-modals/img-03-modal.webp"
+import img04 from "../assets/img-modals/img-04-modal.webp"
+import img05 from "../assets/img-modals/img-05-modal.webp"
+import img06 from "../assets/img-modals/img-06-modal.webp"
+import img07 from "../assets/img-modals/img-07-modal.webp"
+import img08 from "../assets/img-modals/img-08-modal.webp"
+import img09 from "../assets/img-modals/img-09-modal.webp"
+import img10 from "../assets/img-modals/img-10-modal.webp"
+
+const backgroundModals: Record<string, ImageMetadata> = {
+    "01": img01,
+    "02": img02,
+    "03": img03,
+    "04": img04,
+    "05": img05,
+    "06": img06,
+    "07": img07,
+    "08": img08,
+    "09": img09,
+    "10": img10,
+}
+
+
+// IMAGES TITLE MODAL 
+
+import title01 from "../assets/img-title-01.webp"
+import title02 from "../assets/img-title-02.webp"
+import title03 from "../assets/img-title-03.webp"
+import title04 from "../assets/img-title-04.webp"
+import title05 from "../assets/img-title-05.webp"
+import title06 from "../assets/img-title-06.webp"
+import title07 from "../assets/img-title-07.webp"
+import title08 from "../assets/img-title-08.webp"
+import title09 from "../assets/img-title-09.webp"
+import title10 from "../assets/img-title-10.webp"
+
+
+const TITLE_MODALS: Record<string, ImageMetadata> = {
+    "01": title01,
+    "02": title02,
+    "03": title03,
+    "04": title04,
+    "05": title05,
+    "06": title06,
+    "07": title07,
+    "08": title08,
+    "09": title09,
+    "10": title10,
+}
+
+export { backgroundModals, TITLE_MODALS }
